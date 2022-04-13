e7f62bb191a20aea2731b07a02ff2603e213ec2b
MoidulHasan <antu.khan.988@gmail.com>
2022-04-14 02:12:56 +0600

    Dashboard completed

--- user/admin/index.php
+++ user/admin/index.php
@@ -35,6 +35,29 @@ include'./includes/sidebar.php';
                     </div>
                 </div>
 
+                <?php
+                    $query = "SELECT * FROM preusers";
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+                    $totalPreUser = mysqli_num_rows($result);
+                ?>
+                <div class="col-6 col-md-3 my-5">
+                    <div class="card p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-start">
+                                <h3 class="justify-content-start">
+                                     Total Pending User
+                                </h3>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h3 class="justify-content-end">
+                                    <?php  echo $totalPreUser; ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
 
@@ -92,7 +115,106 @@ include'./includes/sidebar.php';
 
 
 
+                <?php
+                    $query = "SELECT * FROM blood_donation WHERE status='Approved'";
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
 
+                    $totalApprovedDonation = mysqli_num_rows($result);
+                ?>
+                <div class="col-6 col-md-3 my-5">
+                    <div class="card p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-start">
+                                <h3 class="justify-content-start">
+                                     Total Approved Blood Donation
+                                </h3>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h3 class="justify-content-end">
+                                    <?php  echo $totalApprovedDonation; ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <?php
+                    $query = "SELECT * FROM blood_donation WHERE status='Pending'";
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+                    $totalPendingDonation = mysqli_num_rows($result);
+                ?>
+                <div class="col-6 col-md-3 my-5">
+                    <div class="card p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-start">
+                                <h3 class="justify-content-start">
+                                     Total Pending Blood Donation
+                                </h3>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h3 class="justify-content-end">
+                                    <?php  echo $totalPendingDonation; ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <?php
+                    $query = "SELECT * FROM blood_request WHERE status='Approved'";
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+                    $totalApprovedRequest = mysqli_num_rows($result);
+                ?>
+                <div class="col-6 col-md-3 my-5">
+                    <div class="card p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-start">
+                                <h3 class="justify-content-start">
+                                     Total Approved Blood Request
+                                </h3>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h3 class="justify-content-end">
+                                    <?php  echo $totalApprovedRequest; ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <?php
+                    $query = "SELECT * FROM blood_request WHERE status='Pending'";
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+                    $totalPendingRequest = mysqli_num_rows($result);
+                ?>
+                <div class="col-6 col-md-3 my-5">
+                    <div class="card p-3">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-start">
+                                <h3 class="justify-content-start">
+                                     Total Pending Blood Request
+                                </h3>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <h3 class="justify-content-end">
+                                    <?php  echo $totalPendingRequest; ?>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
 
 
