a72595d76dced38ae7ebb115395540388c237034
MoidulHasan <antu.khan.988@gmail.com>
2022-04-24 23:42:47 +0600

    pie chart updated

--- user/admin/index.php
+++ user/admin/index.php
@@ -138,6 +138,13 @@ include'./includes/sidebar.php';
             $result = mysqli_query($con, $query) or die (mysqli_error($con));
 
             $totalApprovedDonation = mysqli_num_rows($result);
+
+
+            // find total approved donation blood unit
+            $totalApprovedBloodUnit = 0;
+            while($bloodDonation = mysqli_fetch_assoc($result)) {
+                $totalApprovedBloodUnit += $bloodDonation['unit'];
+            }
         ?>
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
@@ -197,6 +204,13 @@ include'./includes/sidebar.php';
             $result = mysqli_query($con, $query) or die (mysqli_error($con));
 
             $totalApprovedRequest = mysqli_num_rows($result);
+
+
+            // find total approved request blood unit
+            $totalApprovedBloodRequestUnit = 0;
+            while($bloodDonation = mysqli_fetch_assoc($result)) {
+                $totalApprovedBloodRequestUnit += $bloodDonation['unit'];
+            }
         ?>
 
         <!-- Total donor card -->
@@ -252,6 +266,87 @@ include'./includes/sidebar.php';
         </div>
     </div>
 
+
+
+    <!-- Blood Stock Section -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h4 class="m-2 font-weight-bold text-primary">Blood Stock</h4>
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+
+                <?php
+                    $query = "SELECT * FROM blood_stock";
+
+                    // query for preusers data
+                    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                            <div class="col-6 col-md-3 my-5">
+                                <div class="card p-3">
+                                    <div class="row">
+                                        <div class="col-6 d-flex justify-content-start">
+                                            <h3 class="justify-content-start">
+                                                <?php  echo $row["bloodGroup"]; ?>
+                                            </h3>
+                                        </div>
+                                        <div class="col-6 d-flex justify-content-end">
+                                            <h3 class="justify-content-end">
+                                                <?php  echo $row['unit']; ?>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+    
+
+    <?php
+
+        // find total unit of rejected donation
+        $query = "SELECT * FROM blood_donation WHERE status='Rejected'";
+        $result = mysqli_query($con, $query) or die (mysqli_error($con));
+        $totalRejectedDonationUnit = 0;
+        while($bloodDonation = mysqli_fetch_assoc($result)) {
+            $totalRejectedDonationUnit += $bloodDonation['unit'];
+        }
+
+        // find number of rejected Request
+        $query1 = "SELECT * FROM blood_request WHERE status='Rejected'";
+        $result1 = mysqli_query($con, $query1) or die (mysqli_error($con));
+        $totalRejectedBloodRequestUnit = 0;
+        while($bloodDonation = mysqli_fetch_assoc($result1)) {
+            $totalRejectedBloodRequestUnit += $bloodDonation['unit'];
+        }
+
+
+        // find the number of blood unit requested
+        $requestBloodSQL = "SELECT * FROM blood_request WHERE status='Pending'";
+        $requestBloodResult = mysqli_query($con, $requestBloodSQL) or die (mysqli_error($con));
+        $numberOfUnitNeeded = 0;
+        while($bloodRequest = mysqli_fetch_assoc($requestBloodResult)) {
+            $numberOfUnitNeeded += $bloodRequest['unit'];
+        }
+
+        // find the number of blood unit available
+        $donationBloodSQL = "SELECT * FROM blood_donation WHERE status='Pending'";
+        $donationBloodResult = mysqli_query($con, $donationBloodSQL) or die (mysqli_error($con));
+        $numberOfUnitAvailable = 0;
+        while($bloodDonation = mysqli_fetch_assoc($donationBloodResult)) {
+            $numberOfUnitAvailable += $bloodDonation['unit'];
+        }
+    ?>
+
+
     <!-- Pi chart row -->
     <div class="row">
 
@@ -269,10 +364,10 @@ include'./includes/sidebar.php';
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Pending Blood Unit
+                            <i class="fas fa-circle text-primary"></i> Pending Donation Blood Unit - <?php echo $numberOfUnitAvailable ?>
                         </span>
                         <span class="mr-2">
-                            <i class="fas fa-circle text-danger"></i> Requested Blood Unit
+                            <i class="fas fa-circle text-warning"></i> Pending Requested Blood Unit - <?php echo $numberOfUnitNeeded ?>
                         </span>
                     </div>
                 </div>
@@ -293,10 +388,10 @@ include'./includes/sidebar.php';
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Accepted
+                            <i class="fas fa-circle text-primary"></i> Approved Unit - <?php echo $totalApprovedBloodUnit ?>
                         </span>
                         <span class="mr-2">
-                            <i class="fas fa-circle text-danger"></i> Rejected
+                            <i class="fas fa-circle text-danger"></i> Rejected Unit - <?php echo $totalRejectedDonationUnit ?>
                         </span>
                     </div>
                 </div>
@@ -317,10 +412,10 @@ include'./includes/sidebar.php';
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Accepted
+                            <i class="fas fa-circle text-primary"></i> Accepted - <?php echo $totalApprovedBloodRequestUnit ?>
                         </span>
                         <span class="mr-2">
-                            <i class="fas fa-circle text-danger"></i> Rejected
+                            <i class="fas fa-circle text-danger"></i> Rejected - <?php echo $totalRejectedBloodRequestUnit ?>
                         </span>
                     </div>
                 </div>
@@ -341,30 +436,7 @@ include'./includes/sidebar.php';
     </div> -->
     
 
-    <?php
-        $query = "SELECT * FROM blood_donation WHERE status='Rejected'";
-        // query for rejected blood donation data
-        $result = mysqli_query($con, $query) or die (mysqli_error($con));
 
-        $totalRejectedDonation = mysqli_num_rows($result);
-
-
-        // find the number of blood unit requested
-        $requestBloodSQL = "SELECT * FROM blood_request WHERE status='Pending'";
-        $requestBloodResult = mysqli_query($con, $requestBloodSQL) or die (mysqli_error($con));
-        $numberOfUnitNeeded = 0;
-        while($bloodRequest = mysqli_fetch_assoc($requestBloodResult)) {
-            $numberOfUnitNeeded += $bloodRequest['unit'];
-        }
-
-        // find the number of blood unit available
-        $donationBloodSQL = "SELECT * FROM blood_donation WHERE status='Pending'";
-        $donationBloodResult = mysqli_query($con, $requestBloodSQL) or die (mysqli_error($con));
-        $numberOfUnitAvailable = 0;
-        while($bloodDonation = mysqli_fetch_assoc($donationBloodResult)) {
-            $numberOfUnitAvailable += $bloodDonation['unit'];
-        }
-    ?>
 
 
 
@@ -374,11 +446,16 @@ include'./includes/sidebar.php';
         include'./includes/footer.php';
     ?>
 
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script> -->
+
+
 
     <script>
         // Set new default font family and font color to mimic Bootstrap's default styling
-        Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-        Chart.defaults.global.defaultFontColor = '#858796';
+        // Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+        // Chart.defaults.global.defaultFontColor = '#858796';
 
         // function to runder a pi chart
         // this function take chart html id, lavels and data
@@ -411,9 +488,9 @@ include'./includes/sidebar.php';
                 caretPadding: 10,
                 },
                 legend: {
-                display: false
+                    display: false
                 },
-                cutoutPercentage: 80,
+                cutoutPercentage: 60,
             },
             });
 
@@ -422,25 +499,29 @@ include'./includes/sidebar.php';
         
         // Blood stock data
         const stockData = [
-            unitNeeded = <?php echo $numberOfUnitNeeded;?>,
-            unitAvailable = <?php echo $numberOfUnitAvailable;?>
+             <?php echo $numberOfUnitAvailable;?>,
+             <?php echo $numberOfUnitNeeded;?>
         ];
 
-        console.log(stockData);
 
         // calll function to show request vs donation pi chart
-        showPiChart("stockPieChart", ["Available Blood Unit", "Requested Blood Unit"],  stockData, ["#4e73df", "#e0dc6d"], ["#4e73df", "#e5dc6d"]);
+        showPiChart("stockPieChart", ["Pending Donation Blood Unit", "Pending Requested Blood Unit"],  stockData, ["#4e73df", "#e0dc6d"], ["#4e73df", "#e5dc6d"]);
 
         // Blood donation data
-        const data = [
-            approved = <?php echo $totalApprovedDonation;?>,
-            rejected = <?php echo $totalRejectedDonation;?>
+        const donData = [
+            <?php echo $totalApprovedBloodUnit;?>,
+            <?php echo $totalRejectedDonationUnit;?>
         ];
         // call function to show blood donation pi chart
-        showPiChart("bloodDonationPieChart", ["Accepted", "Rejected"],  data, ["#4e73df", "#d33b3b"], ["#4e73df", "#d33b3b"]);
+        showPiChart("bloodDonationPieChart", ["Accepted", "Rejected"],  donData, ["#4e73df", "#d33b3b"], ["#4e73df", "#d33b3b"]);
         
-        // call function to blood request pi chart
-        showPiChart("bloodRequestPieChart", ["Accepted", "Rejected"],  data, ["#4e73df", "#d33b3b"], ["#4e73df", "#d33b3b"]);
+        // Blood Request data
+        const reqData = [
+            <?php echo $totalApprovedBloodRequestUnit;?>,
+            <?php echo $totalRejectedBloodRequestUnit;?>
+        ];
 
-        
+        // call function to blood request pi chart
+        showPiChart("bloodRequestPieChart", ["Accepted", "Rejected"],  reqData, ["#4e73df", "#d33b3b"], ["#4e73df", "#d33b3b"]);
+
 </script>
